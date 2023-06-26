7e4068caa3cb915128552d87f06a3f915f3241a9
James Feehan <pylaserpulse@outlook.com>
2023-06-27 00:13:56 +0100

    On branch readthedocs  Your branch is up-to-date with 'origin/readthedocs'.
     Changes to be committed:
    	new file:   docs/_static/theme.css
    	modified:   docs/conf.py
    trying to make the navigation pane wider in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,11 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
+
+def setup(app):
+    app.add_css_file('theme.css')
+
+
 project = 'pyLaserPulse'
 copyright = '2023, James Feehan'
 author = 'James Feehan'
