56fa224c0de9578b3004c863208c90e8ca357cb7
James Feehan <pylaserpulse@outlook.com>
2023-06-27 00:23:02 +0100

    On branch readthedocs  Your branch is up-to-date with 'origin/readthedocs'.
     Changes to be committed:
    	modified:   conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,5 @@ autodoc_default_options = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
