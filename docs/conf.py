dd2a184d3f8ce32564bdf0714d467db888a5f52d
James Feehan <pylaserpulse@outlook.com>
2023-06-26 02:17:52 +0100

    On branch readthedocs  Your branch is up-to-date with 'origin/readthedocs'.
     Changes to be committed:
    	modified:   docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ release = '0.0.0'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../pyLaserPulse'))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
