On branch bugfix  Changes to be committed: 	modified:   pyLaserPulse/abstract_bases.py Factor of dz in time-domain gain operator Input pulse energy of Esat now reduces the gain by x0.37 over the duration of the pulse.

pyLaserPulse/abstract_bases.py
@@ -1821,7 +1821,7 @@ class active_fibre_base(ABC):
         av_Esat = np.average(self.Esat, weights=spec)
         f = utils.ifft(spec_field)
         P = np.sum(f.real**2 + f.imag**2, axis=0)
-        f *= np.exp(-1 * np.cumsum(P * self.grid.dt / av_Esat) / 2)
+        f *= np.exp(-1 * np.cumsum(P * self.grid.dt / av_Esat) * dz / 2)
         spec_field = utils.fft(f)
         return spec_field
 