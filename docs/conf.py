73f22210f24a2024bda71ca49d8a07779530ac58
James Feehan <42572122+jsfeehan@users.noreply.github.com>
2024-04-11 00:07:29 +0100

    Update conf.py
    readthedocs not building
    
    no theme named 'sphinx_rtd_theme' found (missing theme.conf?)

--- docs/conf.py
+++ docs/conf.py
@@ -38,5 +38,6 @@ autodoc_default_options = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = "classic"
 html_static_path = ['_static']
