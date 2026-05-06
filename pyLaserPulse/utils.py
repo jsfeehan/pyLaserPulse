2db874b7c908b5228bce334f1accf1a95c3ee6d2
James Feehan <pylaserpulse@outlook.com>
2026-05-06 14:12:46 +0100

    On branch new_grid_definition  Your branch is up-to-date with 'origin/new_grid_definition'.
     Changes to be committed:
    	modified:   pyLaserPulse/data/fibres/cross_sections/Yb_Nufern_PM_YSF_HI_HP.dat
    	modified:   pyLaserPulse/utils.py
    
     Untracked files:
    	pyLaserPulse/.test.py.swp
    	pyLaserPulse/.utils.py.swp
    
    Reduced size of Yb_Nufern_PM_YSF_HI_HP cross section file because
    the edges were noisy.
    
    Adjusted utils.load_cross_sections function so that the cross
    sections are now extrapolated when loaded in -- exponential fall
    towards the wavelength grid extremes. Working well. Will remove
    commented code later.

--- pyLaserPulse/utils.py
+++ pyLaserPulse/utils.py
@@ -284,9 +284,9 @@ def load_cross_sections(filename, delimiter, axis, axis_scale,
     min_wl = data[:, 0].min()
     max_wl = data[:, 0].max()
     absorption_func = interp1d(data[:, 0], data[:, 2], kind=interp_kind,
-                               fill_value='extrapolate')
+                               fill_value=0, bounds_error=False)  # 'extrapolate')
     emission_func = interp1d(data[:, 0], data[:, 1], kind=interp_kind,
-                             fill_value='extrapolate')
+                             fill_value=0, bounds_error=False)  # 'extrapolate')
     absorption = absorption_func(axis)
     emission = emission_func(axis)
 
@@ -294,6 +294,72 @@ def load_cross_sections(filename, delimiter, axis, axis_scale,
     # bad values after interpolation. Fix these.
     absorption[absorption < 0] = 0
     emission[emission < 0] = 0
+
+    # exponential fall off either side of the data
+    # Get log data
+    log_abs = np.log(absorption)
+    log_ems = np.log(emission)
+
+    #import matplotlib.pyplot as plt
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)
+    #ax.plot(axis, log_abs)
+    #ax.plot(axis, log_ems)
+
+    # Get log raw data
+    log_raw_abs = np.log(data[:, 2])
+    log_raw_ems = np.log(data[:, 1])
+
+    # Get gradient at the extremes (as average over 5 points)
+    dlog_raw_abs_r = np.average(np.gradient(log_raw_abs, data[:, 0])[0:19])
+    dlog_raw_abs_l = np.average(np.gradient(log_raw_abs, data[:, 0])[-21:-1])
+    dlog_raw_ems_r = np.average(np.gradient(log_raw_ems, data[:, 0])[0:19])
+    dlog_raw_ems_l = np.average(np.gradient(log_raw_ems, data[:, 0])[-21:-1])
+
+    # Get indices of final non-zero values in emission and absorption at the
+    # left and right
+    indices = np.linspace(0, len(axis)-1, len(axis))
+    log_abs_y_l = int(np.amin(indices[absorption > 0]))
+    log_abs_y_r = int(np.amax(indices[absorption > 0]))
+    log_ems_y_l = int(np.amin(indices[emission > 0]))
+    log_ems_y_r = int(np.amax(indices[emission > 0]))
+
+    log_line_abs_l = dlog_raw_abs_l * axis
+    log_line_abs_r = dlog_raw_abs_r * axis
+    log_line_ems_l = dlog_raw_ems_l * axis
+    log_line_ems_r = dlog_raw_ems_r * axis
+
+    log_line_abs_l -= log_line_abs_l[log_abs_y_l] - log_abs[log_abs_y_l]
+    log_line_abs_r -= log_line_abs_r[log_abs_y_r] - log_abs[log_abs_y_r]
+    log_line_ems_l -= log_line_ems_l[log_ems_y_l] - log_ems[log_ems_y_l]
+    log_line_ems_r -= log_line_ems_r[log_ems_y_r] - log_ems[log_ems_y_r]
+
+    log_abs[0:log_abs_y_l] = log_line_abs_l[0:log_abs_y_l]
+    log_abs[log_abs_y_r::] = log_line_abs_r[log_abs_y_r::]
+    log_ems[0:log_ems_y_l] = log_line_ems_l[0:log_ems_y_l]
+    log_ems[log_ems_y_r::] = log_line_ems_r[log_ems_y_r::]
+
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)
+    #ax.plot(axis, log_line_abs_l, c='k')
+    #ax.plot(axis, log_line_abs_r, c='r')
+    #ax.plot(axis, log_line_ems_l, c='k', ls='--')
+    #ax.plot(axis, log_line_ems_r, c='r', ls='--')
+    #ax.legend(['log_line_abs_l', 'log_line_abs_r', 'log_line_ems_l', 'log_line_ems_r'])
+
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)
+    #ax.plot(axis, log_abs, c='k')
+    #ax.plot(axis, log_ems, c='r', ls='--')
+    #ax.plot(data[:,0], log_raw_abs, c='darkorange', ls='--')
+    #ax.plot(data[:,0], log_raw_ems, c='k', ls='--')
+    #ax.legend(['log abs', 'log ems', 'log raw abs', 'log raw ems'])
+    #plt.show()
+
+    absorption[axis > 0] = np.exp(log_abs[axis > 0])
+    emission[axis > 0] = np.exp(log_ems[axis > 0])
+    absorption[axis < 0] = 0
+    emission[axis < 0] = 0
     return absorption, emission, [min_wl, max_wl]
 
 
