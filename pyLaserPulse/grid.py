3faece1b2d175f8b97214caffb798d765375ac85
James Feehan <pylaserpulse@outlook.com>
2026-06-04 15:21:28 +0100

    On branch new_grid_definition  Your branch is up-to-date with 'origin/new_grid_definition'.  Changes to be committed: 	deleted:    examples/saving_and_recalling_data/amp 1/grid.npz 	deleted:    examples/saving_and_recalling_data/amp 1/optical_assembly.npz 	deleted:    examples/saving_and_recalling_data/amp 1/pulse.npz 	modified:   examples/saving_and_recalling_data/amplifier_1.py 	modified:   examples/saving_and_recalling_data/amplifier_2.py 	modified:   pyLaserPulse/grid.py
    Saving and recalling works. Removing data files from repo.

--- pyLaserPulse/grid.py
+++ pyLaserPulse/grid.py
@@ -300,87 +300,127 @@ class grid:
                               2e9 * np.pi * const.c / omega_min))
             print(infostring)
 
+    def save(self, directory):
+        """
+        Save the grid information to a file in directory.
+
+        Parameters
+        ----------
+        directory : string
+            directory to which the data will be saved.
+
+        Notes
+        -----
+        Only information required to recreate the grid object is saved.
+
+        Data is saved using the numpy.savez method. Data can be accessed using
+        the numpy.load method, which will return a dictionary with the
+        following keys:
+        points : Number of data points in the time-frequency grid
+        lambda_c : Central wavelength in m
+        lambda_max : Maximum grid wavelength in m.
+        """
+        np.savez(directory + 'grid.npz',
+                 lambda_c=self.lambda_c,
+                 lambda_lims=self.lambda_lims,
+                 t_range=self.t_range)
+
 
 class grid_from_pyLaserPulse_simulation(grid):
     """
     Time-frequency grid class.
     """
-
     def __init__(self, data_directory):
-        """
-        Parameters
-        ----------
-        data_direcotry : str
-            Absolute path of directory containing grid.npz data file produced
-            by a previous pyLaserPulse simulation.
-
-        Attributes
-        ----------
-        points : int
-            Number of points in the time-frequency grid. Integer power of 2.
-        midpoint : int
-            int(points / 2) -- The central point of the time-frequency grid.
-        lambda_c : float
-            The central wavelength of the frequency grid in m
-        lambda_max : float
-            The maximum wavelength of the frequency grid in m.
-        omega_c : float
-            The central angular frequency of the angular frequency grid in
-            rad Hz.
-        f_range : float
-            The range (or span) of the frequency grid in Hz
-        df : float
-            The resolution of the frequency grid in Hz
-        dOmega : float
-            The resolution of the angular frequency grid in rad Hz
-        t_range : float
-            The range (or span) of the time grid in s
-        dt : float
-            The resolution of the time grid in s
-        time_window : numpy array
-            The time grid in s
-        omega : numpy array
-            The angular frequency grid in rad Hz centred at 0 rad Hz
-        omega_window : numpy array
-            The angular frequency grid in rad Hz
-        omega_window_shift : numpy array
-            The FFTshifted angular frequency grid in rad Hx
-        f_window : numpy array
-            The frequency grid in Hz
-        lambda_window : numpy array
-            The wavelength window in m
-        d_wl : numpy array
-            The resolution of the wavelength window. See notes; the wavelength
-            window is not evenly spaced.
-        energy_window : numpy array
-            The energy window in J, given by Planck's constant * f_window
-        energy_window_shift : numpy array
-            The FFTshifted energy window in J.
-        FFT_scale : float
-            Scaling multiplier or divider for FFTs and IFFts, respectively.
-            FFT_scale = sqrt(2 * pi) / dt
-
-        Notes
-        -----
-        The grids are calculated as follows:
-
-        f_range = 2 (c / lambda_c - c / lambda_max), where c = 299792457 m/s
-        df = f_range / points
-        t_range = 1 / df
-        dt = t_range / points
-        time_window = dt * linspace(-1*midpoint, midpoint-1, points)
-        dOmega = 2 * pi * df
-        omega_window = dOmega * linspace(-1*midpoint, midpoint-1, points)
-        lambda_window = 2 * pi * c / omega_window, where c = 299792458 m/s
-        """
         if not data_directory.endswith(os.sep):
             grid_data = np.load(data_directory + os.sep + 'grid.npz')
         else:
             grid_data = np.load(data_directory + 'grid.npz')
         super().__init__(
-            grid_data['points'],
             grid_data['lambda_c'],
-            grid_data['lambda_max'])
+            grid_data['lambda_lims'],
+            grid_data['t_range'])
+
+
+#class grid_from_pyLaserPulse_simulation(grid):
+#    """
+#    Time-frequency grid class.
+#    """
+#
+#    def __init__(self, data_directory):
+#        """
+#        Parameters
+#        ----------
+#        data_direcotry : str
+#            Absolute path of directory containing grid.npz data file produced
+#            by a previous pyLaserPulse simulation.
+#
+#        Attributes
+#        ----------
+#        points : int
+#            Number of points in the time-frequency grid. Integer power of 2.
+#        midpoint : int
+#            int(points / 2) -- The central point of the time-frequency grid.
+#        lambda_c : float
+#            The central wavelength of the frequency grid in m
+#        lambda_max : float
+#            The maximum wavelength of the frequency grid in m.
+#        omega_c : float
+#            The central angular frequency of the angular frequency grid in
+#            rad Hz.
+#        f_range : float
+#            The range (or span) of the frequency grid in Hz
+#        df : float
+#            The resolution of the frequency grid in Hz
+#        dOmega : float
+#            The resolution of the angular frequency grid in rad Hz
+#        t_range : float
+#            The range (or span) of the time grid in s
+#        dt : float
+#            The resolution of the time grid in s
+#        time_window : numpy array
+#            The time grid in s
+#        omega : numpy array
+#            The angular frequency grid in rad Hz centred at 0 rad Hz
+#        omega_window : numpy array
+#            The angular frequency grid in rad Hz
+#        omega_window_shift : numpy array
+#            The FFTshifted angular frequency grid in rad Hx
+#        f_window : numpy array
+#            The frequency grid in Hz
+#        lambda_window : numpy array
+#            The wavelength window in m
+#        d_wl : numpy array
+#            The resolution of the wavelength window. See notes; the wavelength
+#            window is not evenly spaced.
+#        energy_window : numpy array
+#            The energy window in J, given by Planck's constant * f_window
+#        energy_window_shift : numpy array
+#            The FFTshifted energy window in J.
+#        FFT_scale : float
+#            Scaling multiplier or divider for FFTs and IFFts, respectively.
+#            FFT_scale = sqrt(2 * pi) / dt
+#
+#        Notes
+#        -----
+#        The grids are calculated as follows:
+#
+#        f_range = 2 (c / lambda_c - c / lambda_max), where c = 299792457 m/s
+#        df = f_range / points
+#        t_range = 1 / df
+#        dt = t_range / points
+#        time_window = dt * linspace(-1*midpoint, midpoint-1, points)
+#        dOmega = 2 * pi * df
+#        omega_window = dOmega * linspace(-1*midpoint, midpoint-1, points)
+#        lambda_window = 2 * pi * c / omega_window, where c = 299792458 m/s
+#        """
+#        if not data_directory.endswith(os.sep):
+#            grid_data = np.load(data_directory + os.sep + 'grid.npz')
+#        else:
+#            grid_data = np.load(data_directory + 'grid.npz')
+#        super().__init__(
+#            grid_data['points'],
+#            grid_data['lambda_c'],
+#            grid_data['lambda_max'])
 
 
 if __name__ == "__main__":
